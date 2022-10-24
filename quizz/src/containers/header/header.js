2fc83ee28521038de9a907375833aee970a4ab86
bartek-popielas <bartek_popielas@interia.pl>
2022-10-24 15:52:47 +0200

    Logo_changed

--- quizz/src/containers/header/header.js
+++ quizz/src/containers/header/header.js
@@ -1,11 +1,15 @@
 import { StyledHeader } from './styles'
-import LogoIcon from '../../assets/images/logo_johnywick.jpg'
+import LogoIcon from '../../assets/images/logo_jw.png'
 
 export default function Header() {
     return (
         <StyledHeader>
             <img src={LogoIcon} alt="logo_johnywick"/>
-            <p>Siabadaba!</p>
+            <h2>Pozwól, że poznamy się lepiej.
+                Chciałbym zadać Ci kilka
+                pytań, dzięki którym dowiem
+                się, co uszczęśliwia Cię
+                najbardziej!</h2>
 
         </StyledHeader>
     )
