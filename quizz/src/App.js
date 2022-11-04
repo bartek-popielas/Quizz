a3778c6784b5976d85fc94a70961289c11beb7ea
bartek-popielas <bartek_popielas@interia.pl>
2022-11-04 18:08:31 +0100

    adding_input_values_to_object&&wordpress_API

--- quizz/src/App.js
+++ quizz/src/App.js
@@ -1,11 +1,63 @@
 import './App.css';
 import React from "react";
 import View from "./Components/views/view";
+import {useState, useEffect} from "react";
 
+const url = "https://johnywick.pl/wp-json/wp/v2/product?categories=drzewne";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const response = await fetch(url, {
+      method: "GET",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Unable to Retrieve Data. Please try again.");
+    }
+    return response.json();
+  };
+
+  useEffect(() => {
+    fetchProducts()
+        .then((data) => {
+          setProducts((products) => [...data]);
+        })
+        .catch((err) => {
+          console.error(err.message);
+        });
+  }, []);
+
+  if (!products.length) {
+    return <div>loading</div>;
+  }
+
+
+  const items = products.map((prod) => (
+        <li key={prod.id}>
+          <h1>{prod.slug}</h1>
+
+        </li>
+
+
+
+  ));
+
+  products.map((item) => {
+    console.log(item);
+  })
+
   return (
-    <View/>
+      <>
+        <View/>
+        <ul>{items}</ul>
+      </>
+
+
   );
 }
 
--- quizz/src/Components/views/view.js
+++ quizz/src/Components/views/view.js
@@ -9,21 +9,20 @@ import { StepContext } from "../../context/stepContext";
 export default function View() {
     const [step, setStep] = useState(1);
     const [select, setSelect] = useState("");
-    const [questionBuffor, setQuestionBuffor] = useState(0);
-    const [questionIdBuffor, setQuestionIdBuffor] = useState(0);
-    const [newUser, setNewUser] = useState([{
-        id: "",
-        sex: "",
-        name: "",
-        age: 0,
-        answers: {
-            1: 0,
-            2: 0,
-            3: 0,
-            4: 0,
-            5: 0
-        }}]);
-
+    const [newUser, setNewUser] = useState({
+        users: [{
+            id: "",
+            sex: "",
+            name: "",
+            age: 0,
+            answers: {
+                1: 1,
+                2: 0,
+                3: 0,
+                4: 0,
+                5: 0
+            }
+        }]});
 
     const handleSelectChange = (e) => {
         const value = e.target.id
@@ -32,38 +31,29 @@ export default function View() {
         if(step <= 2) {
 
             setNewUser((prev) => ({
-                ...prev,
-                [e.target.name]: e.target.value,
+                users: [
+                    ...prev.users,
+                    {[e.target.name]: e.target.value},
+                ]
             }));
         } else  {
-            setQuestionBuffor(e.target.value);
-            setQuestionIdBuffor(e.target.id);
-        }
-
-    }
-
-
-    const stepIncrement = () => {
-
-        if(questionBuffor !== 0) {
-            setStep((prev) => prev + 1);
 
             setNewUser((prev) => ({
-                ...prev,
-                answers: {
-                    ...prev.answers,
-                    [questionIdBuffor]: questionBuffor,
-                },
+                users: [
+                    ...prev.users,
+                    {[e.target.id]: e.target.value},
+                ]
             }));
 
-        } else {
-            setStep((prev) => prev + 1);
         }
-
     }
 
 
-    console.log(newUser)
+    const stepIncrement = (e) => {
+        setStep((prev) => prev + 1);
+    }
+
+
         return (
             <Fragment>
                 <GlobalStyles/>
